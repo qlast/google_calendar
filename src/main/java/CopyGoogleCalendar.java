202b1c987bb0563c826031a93445c07c65a601ca
Leks <quantorite@gmail.com>
2023-12-08 23:01:24 +0200

    правки по селектору и версия Selenide

--- src/main/java/CopyGoogleCalendar.java
+++ src/main/java/CopyGoogleCalendar.java
@@ -1,18 +1,16 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.By;
-
 import static com.codeborne.selenide.Selenide.*;
 
 public class CopyGoogleCalendar {
     public static void main(String[] args) {
-        // Устанавливаем настройки Selenide (например, браузер и URL)
-        Configuration.browser = "chrome";
-        //Configuration.startMaximized = true; // Используем метод startMaximized
 
-        // Открываем Google.com
-        open("https://www.google.com");
-        $("[data-pid=23]").pressEnter();
+        // Устанавливаем настройки Selenide
+        Configuration.browser = "chrome";
+
+        open("https://www.google.com"); /** Открываем Google.com*/
+        $(By.xpath("//a[text()='Почта']")).click();
         Selenide.sleep(5000);
 
         // Добавьте код для выполнения дополнительных действий, если необходимо
